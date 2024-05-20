b256a1b0ec9b7afe67f26c4e51d008e5562d5eb1
akrutm <akrutm813@gmail.com>
2024-05-20 21:55:07 +0900

    added memo.js

---
+++ js/memo.js
@@ -0,0 +1,39 @@
+
+    //1.Save クリックイベント
+
+    $("#save").on("click", function () {
+        const key = $("#title").val();
+        const value = $("#text").val();
+  
+        localStorage.setItem(key, value);
+        const html = `
+        <li>
+          <p>${key}</p>
+          <p>${value}</p>
+        </li>
+      `;
+        $("#list").append(html);
+      });
+  
+  
+  
+      //2.clear クリックイベント
+      $("#clear").on("click", function () {
+        localStorage.clear();
+        $("#list").empty();
+      });
+  
+  
+      //3.ページ読み込み：保存データ取得表示
+      for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const value = localStorage.getItem(key);
+        const html = `
+        <li>
+          <p>${key}</p>
+          <p>${value}</p>
+        </li>
+      `;
+        $("#list").append(html);
+      }
+  
